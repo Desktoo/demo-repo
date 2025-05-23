beacfc8be96c8c6d0ab16279d75da7f70d6cf3ee
Desktoo <akshatkri1463@gmail.com>
2025-05-23 13:11:10 +0530

    did my first commit in the feature_update_branch

--- app/page.js
+++ app/page.js
@@ -2,6 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Hello world</div>
+    <div>
+      <h1>This is a heading tag h1</h1>
+      <div>this is a div tag</div>
+    </div>
   );
 }
